5a604e002a7a7f033fc2847799494e61f1105d97
Maryess <vip.ivan.ivanov2003@bk.ru>
2024-10-08 11:21:43 +0300

    fix problem to connect

--- logo.js
+++ logo.js
@@ -1,31 +1,29 @@
 import * as THREE from 'three';
 
-const width = window.innerWidth,
-	height = window.innerHeight;
-
-// init
-
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-camera.position.z = 1;
-
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(width, height);
-renderer.setAnimationLoop(animate);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// animation
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-function animate(time) {
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+camera.position.z = 5;
 
+function animate() {
+	requestAnimationFrame(animate);
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 	renderer.render(scene, camera);
 }
+
+animate();
